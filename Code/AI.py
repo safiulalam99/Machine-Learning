4ad91266c997839e4574de768e174682b8de53e4
safiulalam99 <safiulalam99>
2022-09-13 09:08:07 +0300

    main

--- Code/AI.py
+++ Code/AI.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
-
+# read data
 
 df = pd.read_csv(r'C:\Users\saifs\Downloads\startup.csv')
 new = pd.read_csv(r'C:\Users\saifs\Downloads\new_company.csv')
